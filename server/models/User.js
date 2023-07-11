ced5bc2d6d6b11a3c394fd7ef167628054158094
Jonathan Schuh <122762581+jonschuh@users.noreply.github.com~>
2023-07-10 21:59:19 -0500

    added userSeed.js and userSeeds.json

--- server/models/User.js
+++ server/models/User.js
@@ -41,4 +41,4 @@ userSchema.methods.isCorrectPassword = async function (password) {
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
