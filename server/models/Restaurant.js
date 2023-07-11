f62af47cc01ed2608d88c012e13a0bda18b8c45a
Saymiki <mikaela.montesclaros@gmail.com>
2023-07-10 19:55:01 -0500

    index nad restaurant updated

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -1,35 +1,28 @@
+const restaurantSchema = new mongoose.Schema({
+name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  address: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  phoneNumber: {
+    type: String,
+    trim: true,
+  },
+ 
+});
 
-// const { Schema, model } = require('mongoose');
-// const bcrypt = require('bcrypt')
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
-// const restaurantSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     trim: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     match: [/.+@.+\..+/, 'Must match an email address!'],
-//   },
-//   address: {
-//     type: String,
-//     required: true,
-//     minlength: 5,
-//   },
-//   phoneNumber: {
-//     type: String,
-//     trim: true,
-//   },
-
-// });
-  
-
-  
-//   const restaurant = model('restaurant', restaurantSchema);
-  
-//   module.exports = restaurant;
-  
+module.exports = mongoose.model('Restaurant', restaurantSchema);
--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,9 @@
 const Event = require('./Event');
+const Restaurant = require('./Restaurant');
+const User = require('./User');
 
-module.exports = Event;
-
+module.exports = {
+  Event,
+  Restaurant,
+  User,
+};
