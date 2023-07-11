87e597d08fc116d967cad6de38b39eca41abe128
Jonathan Schuh <122762581+jonschuh@users.noreply.github.com~>
2023-07-10 19:58:55 -0500

    added type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,18 +7,45 @@ const typeDefs = gql`
     description: String
   }
 
+  type Restaurant {
+    _id: ID
+    name: String
+    description: String
+    email: String
+    address: String
+    phoneNumber: String
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    
+  }
+
   type Query {
     events: [Event]
     event(_id: ID!): Event
+    restaurants: [Restaurant]
+    restaurant(_id: ID!): Restaurant
+    users: [User]
+    user(_id: ID!): User
   }
 
   type Mutation {
     createEvent(title: String!, description: String!): Event
     updateEvent(_id: ID!, title: String, description: String): Event
     deleteEvent(_id: ID!): Event
+    createRestaurant(name: String!, description: String!, email: String!, address: String!, phoneNumber: String): Restaurant
+    updateRestaurant(_id: ID!, name: String, description: String, email: String, address: String, phoneNumber: String): Restaurant
+    deleteRestaurant(_id: ID!): Restaurant
+    createUser(firstName: String!, lastName: String!, email: String!, password: String!): User
+    updateUser(_id: ID!, firstName: String, lastName: String, email: String, password: String): User
+    deleteUser(_id: ID!): User
   }
 `;
 
 module.exports = typeDefs;
-
-
+;
