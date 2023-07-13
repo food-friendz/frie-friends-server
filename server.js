6de929caf5e870c940f072b203f9894ea4089bc3
Saymiki <mikaela.montesclaros@gmail.com>
2023-07-13 16:00:03 -0500

    Fix: redirect on index

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.redirect(process.env.CLIENT_URI ?? "http://localhost:3000");
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
